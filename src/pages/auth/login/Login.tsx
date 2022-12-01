9a4f639dc81632b84027b4bc60245aaab7b78067
yahyo <106180239+yahyo22@users.noreply.github.com>
2022-12-01 18:57:07 +0500

    dsdsdsdsds

---
+++ src/pages/auth/login/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '../../../components/button/Button'
+import styled from 'styled-components'
+export default function Login() {
+  return (
+    <StyledLogin>
+      
+    </StyledLogin>
+  )
+}
+const StyledLogin = styled.div`
+  
+`
